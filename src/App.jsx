bed20121358f6ff9117414b9429e48eadfae6819
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2026-04-06 13:39:32 +0300

    Filter and Numbers
    In this update was added to filter your contacts and you can write a number of this contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,44 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
     name: "",
+    number: "",
   };
 
   handleChange = (evt) => {
-    this.setState({
-      name: evt.target.value,
-    });
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
   };
-
   handleSubmit = (evt) => {
     evt.preventDefault();
     const newContact = {
       id: nanoid(),
       name: this.state.name,
+      number: this.state.number,
     };
     this.setState((prevState) => {
       return {
         contacts: [...prevState.contacts, newContact],
+        name: "",
+        number: "",
       };
     });
   };
 
   render() {
-    const { contacts, name } = this.state;
+    const { contacts, name, number, filter } = this.state;
+    const normalizedFilter = this.state.filter.toLowerCase();
+
+    const filteredContacts = this.state.contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
     return (
       <div>
         <h1>Phone Book</h1>
@@ -44,12 +57,36 @@ export default class App extends Component {
               required
             />
           </label>
+          <label>
+            {" "}
+            Number:
+            <input
+              type="tel"
+              name="number"
+              onChange={this.handleChange}
+              value={number}
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
           <button type="submit">Add Contact</button>
         </form>
         <h2>Contacts:</h2>
+        <input
+          type="text"
+          name="filter"
+          value={filter}
+          title="Filter"
+          onChange={this.handleChange}
+          required
+        />
         <ul>
-          {contacts.map((evt) => {
-            return <li key={evt.id}>{evt.name}</li>;
+          {filteredContacts.map((evt) => {
+            return (
+              <li key={evt.id}>
+                {evt.name}: {evt.number}
+              </li>
+            );
           })}
         </ul>
       </div>
